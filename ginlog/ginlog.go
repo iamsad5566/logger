c83f2a5d258c1c3ec106c34a5e9d8922508ee7c1
Yen-Kuang-tw <nf8964p5566@gmail.com>
2023-11-21 00:45:28 +0800

    feat: update encode level

--- ginlog/ginlog.go
+++ ginlog/ginlog.go
@@ -64,7 +64,7 @@ func getEncoder() zapcore.Encoder {
 	encoderConfig := zap.NewProductionEncoderConfig()
 	encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
 	encoderConfig.TimeKey = "time"
-	encoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
+	encoderConfig.EncodeLevel = zapcore.CapitalLevelEncoder
 	encoderConfig.EncodeCaller = zapcore.ShortCallerEncoder
 	return zapcore.NewJSONEncoder(encoderConfig)
 }
